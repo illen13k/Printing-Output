1f3b2a59c09103528e830c4badad676289e589d1
illen13k <155986651+illen13k@users.noreply.github.com>
2024-01-19 12:35:11 -0500

    Add files via upload

---
+++ exercixe.js
@@ -0,0 +1,39 @@
+// TASK 1
+
+let a = 3;
+let b = 5;
+let c;
+
+sentence = "let a = 3;";
+sentence += "\nlet b = 5;";
+sentence += "\nlet c;";
+sentence += "\n-------------";
+sentence += "\na + b = " + (a + b) + ";";
+sentence += "\na - b = " + (a - b) + ";";
+sentence += "\na * b = " + (a * b) + ";";
+sentence += "\na / b = " + (a / b) + ";";
+sentence += "\na % b = " + (a % b) + ";";
+sentence += "\na += b = " + (a += b) + ";";
+sentence += "\na -= b = " + (a -= b) + ";";
+sentence += "\na *= b = " + (a *= b) + ";";
+sentence += "\na /= b = " + (a /= b) + ";";
+sentence += "\na %= b = " + (a %= b) + ";";
+sentence += "\na == b = " + (a == b) + ";";
+sentence += "\na != b = " + (a != b) + ";";
+sentence += "\na > b = " + (a > b) + ";";
+sentence += "\na < b = " + (a < b) + ";";
+sentence += "\n!a && !c  = " + (!a && !c) + ";";
+sentence += "\n!a || !c   = " + (!a || !c) + ";";
+sentence += "\n";
+sentence += "\n";
+
+console.log(sentence);
+
+// TASK 2
+
+let first_name = "Illen";
+let last_name = "Abraham";
+let email = "abra0196@algonquinlive.com";
+let output = sentence = "My name is " + first_name + " " + last_name + ". You can contact me at " + email + ".";
+
+console.log(output)
